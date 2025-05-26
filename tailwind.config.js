f1ba02b5c306aa449c95980a34f0d3d30b95fd66
karolids <peter@orthotica.com>
2025-05-25 20:57:25 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        orthoticaPink: "#F27497",
-        orthoticaGray: "#5B6670",
-        orthoticaBlack: "#000000",
+        orthoticaPink: '#F27497',
+        orthoticaGray: '#5B6670',
+        orthoticaBlack: '#000000',
       },
     },
   },
   plugins: [require('@tailwindcss/typography')],
-};
+}
